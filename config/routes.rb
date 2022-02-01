a49c13783fe661f46e44a3f3b534bad4741e1925
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-01 23:52:32 +0100

    Create a  Ruby on Rails

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get 'welcome/home', to: 'welcome#home'
+  get 'welcome/about', to: 'welcome#about'
+end
