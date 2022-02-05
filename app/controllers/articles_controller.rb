6d928cfcd32ef26ed231176a4709c4ce4fe40a2e
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-05 16:28:50 +0100

    Add new article method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,14 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
